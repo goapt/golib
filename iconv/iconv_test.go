e84d6354487df13747c628677582f3dd42a7155a
fifsky <fifsky@gmail.com>
2017-11-16 16:51:43 +0800

    add iconv gbktoutf8

---
+++ iconv/iconv_test.go
@@ -0,0 +1,33 @@
+package util
+
+import (
+	"fmt"
+	"testing"
+)
+
+var s string = "GBK与UTF-8 编码转换测试"
+
+func TestGbkToUtf8(t *testing.T) {
+	gbk, err := Utf8ToGbk([]byte(s))
+	if err != nil {
+		t.Error(err)
+	}
+
+	utf8, err := GbkToUtf8([]byte(gbk))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if string(utf8) != s {
+		t.Error("Iconv Fail:", utf8, s)
+	}
+}
+
+func TestUtf8ToGbk(t *testing.T) {
+	gbk, err := Utf8ToGbk([]byte(s))
+	if err != nil {
+		t.Error(err)
+	}
+
+	fmt.Printf("%s", string(gbk))
+}
