0d522560c0422d7113806d5436fac60c96b82d72
huqi <343882515@qq.com>
2017-12-05 13:46:59 +0800

    nsq context

--- nsq/consumer.go
+++ nsq/consumer.go
@@ -3,14 +3,21 @@ package nsq
 import (
 	gonsq "github.com/nsqio/go-nsq"
 	"github.com/pkg/errors"
+	"context"
+	"github.com/verystar/golib/logger"
+	"fmt"
+	"github.com/verystar/golib/color"
 )
 
 type NsqConsumer struct {
 	Consumer *gonsq.Consumer
 	Handlers []gonsq.Handler
+	ctx      context.Context
+	topic    string
+	channel  string
 }
 
-func NewNsqConsumer(topic, channel string, options ...func(*gonsq.Config)) (*NsqConsumer, error) {
+func NewNsqConsumer(ctx context.Context, topic, channel string, options ...func(*gonsq.Config)) (*NsqConsumer, error) {
 	conf := gonsq.NewConfig()
 	conf.MaxAttempts = 0
 
@@ -24,6 +31,9 @@ func NewNsqConsumer(topic, channel string, options ...func(*gonsq.Config)) (*Nsq
 	}
 	return &NsqConsumer{
 		Consumer: consumer,
+		ctx:      ctx,
+		topic:    topic,
+		channel:  channel,
 	}, nil
 }
 
@@ -31,13 +41,24 @@ func (this *NsqConsumer) AddHandler(handler gonsq.Handler) {
 	this.Handlers = append(this.Handlers, handler)
 }
 
-func (this *NsqConsumer) Run(conf Config) error {
+func (this *NsqConsumer) Run(conf Config) {
 	if len(this.Handlers) == 0 {
 		errors.New("Handler Is Empty")
 	}
 	for _, handler := range this.Handlers {
 		this.Consumer.AddHandler(handler)
 	}
-
-	return this.Consumer.ConnectToNSQD(conf.Host + ":" + conf.Port)
+	if err := this.Consumer.ConnectToNSQD(conf.Host + ":" + conf.Port); err != nil {
+		logger.Error("nsq:ConnectToNSQD", err)
+		return
+	}
+	for {
+		select {
+		case <-this.ctx.Done():
+			fmt.Println(color.Yellow("[%s] %s,%s","stop consumer", this.topic, this.channel))
+			this.Consumer.Stop()
+			fmt.Println(color.Yellow("[%s] %s,%s" , "stop consumer success", this.topic, this.channel))
+			return
+		}
+	}
 }
