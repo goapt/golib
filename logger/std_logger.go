ecb41c6656bb57a2dbdb808a1ec96ba03b0809f5
fifsky <fifsky@gmail.com>
2018-03-14 18:22:54 +0800

    std log output Lshortfile

--- logger/std_logger.go
+++ logger/std_logger.go
@@ -1,7 +1,9 @@
 package logger
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/sirupsen/logrus"
 )
@@ -9,30 +11,33 @@ import (
 var _ ILogger = (*StdLogger)(nil)
 
 type StdLogger struct {
+	lg *log.Logger
 }
 
 func NewStdLogger() ILogger {
-	return &StdLogger{}
+	return &StdLogger{
+		lg: log.New(os.Stderr, "", log.Lshortfile | log.LstdFlags),
+	}
 }
 
 func (l *StdLogger) Fatal(format string, args ...interface{}) {
-	log.Printf(format, args...)
+	l.lg.Output(3,fmt.Sprintf(format, args...))
 }
 
 func (l *StdLogger) Debug(format string, args ...interface{}) {
-	log.Printf(format, args...)
+	l.lg.Output(3,fmt.Sprintf(format, args...))
 }
 
 func (l *StdLogger) Info(format string, args ...interface{}) {
-	log.Printf(format, args...)
+	l.lg.Output(3,fmt.Sprintf(format, args...))
 }
 
 func (l *StdLogger) Error(format string, args ...interface{}) {
-	log.Printf(format, args...)
+	l.lg.Output(3,fmt.Sprintf(format, args...))
 }
 
 func (l *StdLogger) Log(level string, format string, args ...interface{}) {
-	log.Printf(format, args...)
+	l.lg.Output(3,fmt.Sprintf(format, args...))
 }
 
 func (l *StdLogger) SetFormatter(format logrus.Formatter) {
