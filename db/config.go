678ca89f4b1fcf9eaa53fae8660313035e069edd
huqi <qi.hu@verystar.cn>
2018-05-09 11:54:21 +0800

    add db config json tag

--- db/config.go
+++ db/config.go
@@ -3,16 +3,17 @@ package db
 import (
 	upperdb "github.com/verystar/db"
 )
+
 type Config struct {
-	Enable       bool
-	Driver       string
-	Dsn          string
-	MaxOpenConns int  `toml:"max_open_conns"`
-	MaxIdleConns int  `toml:"max_idle_conns"`
-	ShowSql      bool `toml:"show_sql"`
+	Enable       bool   `json:"enable"`
+	Driver       string `json:"driver"`
+	Dsn          string `json:"dsn"`
+	MaxOpenConns int    `toml:"max_open_conns" json:"max_open_conns"`
+	MaxIdleConns int    `toml:"max_idle_conns" json:"max_idle_conns"`
+	ShowSql      bool   `toml:"show_sql" json:"show_sql"`
 }
 
 // Error messages.
 var (
-	ErrNoMoreRows               = upperdb.ErrNoMoreRows
+	ErrNoMoreRows = upperdb.ErrNoMoreRows
 )
