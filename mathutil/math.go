e27c8206e94c2b7ab46b70c3191bc4ad5885f929
fifsky <fifsky@gmail.com>
2018-05-31 17:46:42 +0800

    mathutil.Round suggest use math.Round

--- mathutil/math.go
+++ mathutil/math.go
@@ -19,6 +19,8 @@ func PowInt(x int, y int) int {
 	}
 }
 
+//deprecated
+//golang 1.10 or above please use math.Round
 func Round(f float64) (int, error) {
 	return strconv.Atoi(fmt.Sprintf("%.f", f))
 }
