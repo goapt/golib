9507e3cfa136471e75b6af58f26e4ef07399538c
fifsky <fifsky@gmail.com>
2018-06-05 21:19:14 +0800

    remove verystar db dependent

--- db/config.go
+++ db/config.go
@@ -1,9 +1,5 @@
 package db
 
-import (
-	upperdb "github.com/verystar/db"
-)
-
 type Config struct {
 	Enable       bool   `json:"enable"`
 	Driver       string `json:"driver"`
@@ -12,8 +8,3 @@ type Config struct {
 	MaxIdleConns int    `toml:"max_idle_conns" json:"max_idle_conns"`
 	ShowSql      bool   `toml:"show_sql" json:"show_sql"`
 }
-
-// Error messages.
-var (
-	ErrNoMoreRows = upperdb.ErrNoMoreRows
-)
