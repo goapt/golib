beb13c349f277d59cabcc7f6786c4c0b7db30b8a
fifsky <fifsky@gmail.com>
2018-06-06 22:44:05 +0800

    add test

--- convert/gjson.go
+++ convert/gjson.go
@@ -4,15 +4,15 @@ import "github.com/tidwall/gjson"
 
 type JsonTo string
 
-func (this JsonTo) MapString() map[string]string {
-	r := gjson.Parse(string(this))
+func (j JsonTo) MapString() map[string]string {
+	r := gjson.Parse(string(j))
 	return toSSMap(r)
 }
 
 type JsonBytesTo []byte
 
-func (this JsonBytesTo) MapString() map[string]string {
-	r := gjson.ParseBytes([]byte(this))
+func (j JsonBytesTo) MapString() map[string]string {
+	r := gjson.ParseBytes([]byte(j))
 	return toSSMap(r)
 }
 
--- do/struct_test.go
+++ do/struct_test.go
@@ -15,7 +15,7 @@ func TestH_MustJSON(t *testing.T) {
 		},
 	}).MustJSON())
 
-	if str != "{\"a\":1,\"c\":2,\"ddd\":{\"aa\":1,\"cc\":2}}" {
+	if str != `{"a":1,"c":2,"ddd":{"aa":1,"cc":2}}` {
 		t.Error("Result mismatch:" + str)
 	}
 }
