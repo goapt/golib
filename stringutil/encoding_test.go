da2ca453aefd77da71d65eada9e3b6783d70d4b9
fifsky <fifsky@gmail.com>
2018-06-07 10:53:59 +0800

    fix encoding test

--- stringutil/encoding_test.go
+++ stringutil/encoding_test.go
@@ -13,12 +13,12 @@ var (
 
 func TestBase64Encode(t *testing.T) {
 	s := Base64Encode([]byte(originstr))
-	assert.Equal(t, s, base64str, "Base64Encode must be %s but return $s", base64str, s)
+	assert.Equal(t, s, base64str, "Base64Encode must be %s but return %s", base64str, s)
 }
 
 func TestBase64Decode(t *testing.T) {
 	s, err := Base64Decode(base64str)
 
-	assert.Error(t, err, "Base64Decode error %s", err)
-	assert.Equal(t, string(s), base64str, "Base64Decode must be %s but return $s", originstr, string(s))
+	assert.NoError(t, err, "Base64Decode error %s", err)
+	assert.Equal(t, string(s), originstr, "Base64Decode must be %s but return %s", originstr, string(s))
 }
