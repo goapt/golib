ce045a87d8a1c41dd7193fc2f435acd884171399
fifsky <fifsky@gmail.com>
2018-06-20 10:45:34 +0800

    add crontab test

--- crontab/crontab_test.go
+++ crontab/crontab_test.go
@@ -9,16 +9,25 @@ import (
 func TestCrontab_IsDue(t *testing.T) {
 
 	exps := map[string]string{
-		"@every 1m":  "",
-		"@every 2m":  "",
-		"@every 3m":  "",
-		"@every 65m": "",
-		"@every 5m":  "",
-		"@every 3h":  "",
-		"18:53":      "",
+		"@every 1m":  "2012-10-24 07:47:01",
+		"@every 2m":  "2012-10-24 07:02:01",
+		"@every 3m":  "2012-10-24 07:03:01",
+		"@every 1h":  "2012-10-24 07:00:01",
+		"@every 2h":  "2012-10-24 08:00:01",
+		"18:53":      "2012-10-24 18:53:01",
 	}
 
-	fmt.Println(exps)
+	cn := NewCrontab()
+
+	for k, v := range exps {
+		cn.Date = k
+		tt, _ := time.ParseInLocation("2006-01-02 15:04:05", v, time.Local)
+		ok := cn.IsDue(tt)
+
+		if !ok {
+			t.Errorf("Crontab not match:%s->%s", k, v)
+		}
+	}
 }
 
 func TestCrontab_NewCrontab(t *testing.T) {
@@ -27,7 +36,7 @@ func TestCrontab_NewCrontab(t *testing.T) {
 		cron.Date = "@every 1m"
 
 		cron.SetHandle(func(ctx interface{}) {
-			fmt.Println(time.Now().Format("2006-01-02 13:04:05"))
+			fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
 		})
 	})
 
